7b652951c395446c13a7bc813f0978c41678fe6c
Souza <yuri.ramos.f.souza@accenture.com>
2022-09-18 22:59:19 -0300

    primeiro commit

---
+++ TotaldeVotos/Entities/Votos.cs
@@ -0,0 +1,45 @@
+﻿namespace TotaldeVotos.Entities
+{
+    public class Votos
+    {
+        private const double TotalDeEleitores = 1000.0;
+        private const double Validos = 800.0;
+        private const double VotosBrancos = 150.0;
+        private const double Nulos = 50.0;
+
+        /// <summary>
+        /// percentual de votos válidos em relação ao total de eleitores
+        /// </summary>
+        /// <returns></returns>
+        public double PercentualVotosValidos()
+        {
+            double total = Validos / TotalDeEleitores;
+
+            return total * 100;
+        }
+
+        /// <summary>
+        /// precentual de brancos em relação ao total de eleitores
+        /// </summary>
+        /// <returns></returns>
+        public double PercentualVotosBrancos()
+        {
+            double total = VotosBrancos / TotalDeEleitores;
+
+            return total * 100;
+        }
+
+        /// <summary>
+        /// percentual de nulos em relação ao total de eleitores
+        /// </summary>
+        /// <returns></returns>
+        public double PercentualVotosNulos()
+        {
+            double total = Nulos / TotalDeEleitores;
+
+            return total * 100;
+        }
+
+
+    }
+}
---
+++ TotaldeVotos/Program.cs
@@ -0,0 +1,23 @@
+﻿using TotaldeVotos.Entities;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("     Votos em Relação ao Total de Eleitores");
+            Console.WriteLine();
+
+            Votos votos = new Votos();
+
+            Console.WriteLine("  Percentual de votos válidos: " + votos.PercentualVotosValidos() + "%");
+            Console.WriteLine("  Percentual de votos brancos: " + votos.PercentualVotosBrancos() + "%");
+            Console.WriteLine("  Percentual de votos nulos: " + votos.PercentualVotosNulos() + "%");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
